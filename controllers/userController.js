d5cca9b724a333983d570c29b2afbae820756aad
Jeremy-Nourri <nourri.jer@gmail.com>
2024-02-02 11:44:40 +0100

    fix: modify response to send userName and email only

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
 
 const Users = require('../models/userModel');
 
@@ -7,18 +7,18 @@ const userController = {
     createUser: async (req, res) => {
         try {
             const userData = req.body;
-
             const salt = await bcrypt.genSalt(10);
             const hashedPassword = await bcrypt.hash(userData.password, salt);
             userData.password = hashedPassword;
 
-            const user = await Users.create(userData);
-            res.status(201).json(user);
+            const { username, email } = await Users.create(userData);
+            res.status(201).json({ username, email});
         }
-        catch(error) {
+        catch (error) {
             res.status(400).json({ error: "Erreur lors de la création d'un nouvel utilisateur" });
         }
-    }
+    },
+
 }
 
 module.exports = userController;

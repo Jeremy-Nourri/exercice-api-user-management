412bdc97452528acf3b9c65c4cbafeb6cc54b5fc
Jeremy-Nourri <nourri.jer@gmail.com>
2024-02-06 12:15:58 +0100

    fix: verify token middleware, checks the presence of the token

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -2,11 +2,12 @@ const jwt = require("jsonwebtoken");
 
 function verifyToken(req, res, next) {
     const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).send('Token non fourni');
-    }
 
     const token = authHeader.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).send('Token non fourni');
+    }
 
     try {
         const decoded = jwt.verify(token, process.env.RANDOM_TOKEN_SECRET);
